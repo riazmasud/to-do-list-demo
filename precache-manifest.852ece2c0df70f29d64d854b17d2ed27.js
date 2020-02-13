cd5e814a292e8066a5491fd1fdd8b6cf588032ea
riazmasud <masud.riaz@gmail.com>
2020-02-12 21:35:33 -0500

    Updates

---
+++ precache-manifest.852ece2c0df70f29d64d854b17d2ed27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a49c19d359990780beba849ab0411a",
+    "url": "/to-do-list-demo/index.html"
+  },
+  {
+    "revision": "b80785ee617529c70d99",
+    "url": "/to-do-list-demo/static/css/main.c9b2adfd.chunk.css"
+  },
+  {
+    "revision": "c599f57c2a4a9b56f254",
+    "url": "/to-do-list-demo/static/js/2.82350890.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do-list-demo/static/js/2.82350890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80785ee617529c70d99",
+    "url": "/to-do-list-demo/static/js/main.5f42454e.chunk.js"
+  },
+  {
+    "revision": "4f14744fe03d728d55f2",
+    "url": "/to-do-list-demo/static/js/runtime-main.4c60cb47.js"
+  }
+]);
